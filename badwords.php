fbfeb2136dd8958727b2f095e093c7dea92caf12
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-11 16:22:06 +0100

    add: complete project

--- badwords.php
+++ badwords.php
@@ -1,16 +1,44 @@
 <?php
-
+    $paragraph  = $_GET['paragraph'];
+    $censorship = $_GET['censorship'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Censored words</title>
-</head>
-<body>
-
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Censored words</title>
+    </head>
+    <body>
+        <!-- Stampo paragrafo -->
+        <h2>Il tuo paragrafo</h2>
+        <p>
+            <?php 
+            echo $paragraph;
+            ?>
+        </p>
+        <!-- Stampo lunghezza paragrafo tramite strlen-->
+        <h4>Lunghezza del paragrafo</h4>
+        <p>
+            <?php 
+            echo strlen($paragraph);
+            ?>
+        </p>
+        <!-- Sostituisco la parola selezionata tramite str_replace e stampo il nuovo paragrafo censurato -->
+        <h2>Il paragrafo censurato</h2>
+        <p>
+            <?php
+            $censored_paragraph = str_replace($censorship, '***', $paragraph);
+            echo $censored_paragraph;
+            ?>
+        </p>
+        <!-- Stampo la lunghezza del nuovo paragrafo tramite strlen-->
+        <h4>Lunghezza del paragrafo censurato</h4>
+        <p>
+            <?php
+            echo strlen($censored_paragraph);
+            ?>
+        </p>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>Bad words</title>
 </head>
 <body>
-    
+    <form action="badwords.php">
+        <div>
+            <label for="paragraph">Inserisci qui il tuo paragrafo</label>
+            <input type="text" id="paragraph" name="paragraph">
+        </div>
+        
+        <div>
+            <label for="censorship">Inserisci qui la parola che vuoi censurare</label>
+            <input type="text" id="censorship" name="censorship">
+        </div>
+        
+        <button type="submit">Censura</button>
+    </form>
 </body>
 </html>
